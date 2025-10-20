1612ce33d36af560813023c8832d6ed901c28c3e
MiM <mohamed.ismail@ptco.com.sa>
2025-10-20 14:28:08 +0300

    mim

--- electronic_invoice/events/accounts/sales_invoice.py
+++ electronic_invoice/events/accounts/sales_invoice.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import frappe
 from erpnext import get_region
+from helpdesk.api import doc
 from pyqrcode import create as qr_create
 import io
 import os
@@ -8,168 +9,169 @@ from base64 import b64encode
 from frappe import _
 from frappe.utils.data import add_to_date, get_time, getdate,flt
 
-def create_qr_code(doc, method):
-	"""Create QR Code after inserting Sales Inv
-	"""
+def create_qr_codes(doc, method):
+    """Create QR Code after inserting Sales Inv
+    """
 
-	region = get_region(doc.company)
-	if region not in ['Saudi Arabia']:
-		return
-	# if QR Code field not present, do nothing
-	if not hasattr(doc, 'qr_code'):
-		return
+    region = get_region(doc.company)
+    company = frappe.get_doc("Company",doc.company)
 
-	# Don't create QR Code if it already exists
-	qr_code = doc.get("qr_code")
-	# if qr_code and frappe.db.exists({"doctype": "File", "file_url": qr_code}):
-	# 	return
+    if region not in ['Saudi Arabia']:
+        return
+    # if QR Code field not present, do nothing
+    if not hasattr(doc, 'qr_code'):
+        return
 
-	meta = frappe.get_meta('Sales Invoice')
+    # Don't create QR Code if it already exists
+    qr_code = doc.get("qr_code")
+    # if qr_code and frappe.db.exists({"doctype": "File", "file_url": qr_code}):
+    # 	return
 
-	for field in meta.fields:
-		if field.fieldname == 'qr_code':
-			from urllib.parse import urlencode
-			''' TLV conversion for
-			1. Seller's Name
-			2. VAT Number
-			3. Time Stamp
-			4. Invoice Amount
-			5. VAT Amount
-			'''
-			tlv_array = []
-			# Sellers Name
+    meta = frappe.get_meta('Sales Invoice')
 
-			'''TODO: Fix arabic conversion'''
-			# seller_name = frappe.db.get_value(
-			# 	'Company',
-			# 	doc.company,
-			# 	'company_name_in_arabic')
+    for field in meta.fields:
+        if field.fieldname == 'qr_code':
+            from urllib.parse import urlencode
+            ''' TLV conversion for
+            1. Seller's Name
+            2. VAT Number
+            3. Time Stamp
+            4. Invoice Amount
+            5. VAT Amount
+            '''
+            tlv_array = []
+            # Sellers Name
 
-			# if not seller_name:
-			# 	frappe.throw(_('Arabic name missing for {} in the company document'.format(doc.company)))
+            '''TODO: Fix arabic conversion'''
+            # seller_name = frappe.db.get_value(
+            # 	'Company',
+            # 	doc.company,
+            # 	'company_name_in_arabic')
 
-			# seller_name = doc.company
-			# tag = bytes([1]).hex()
-			# length = bytes([len(seller_name)]).hex()
-			# value = seller_name.encode('utf-8').hex()
-			#
-			# tlv_array.append(''.join([tag, length, value]))
+            # if not seller_name:
+            # 	frappe.throw(_('Arabic name missing for {} in the company document'.format(doc.company)))
 
-			# VAT Number
-			tax_id = frappe.db.get_value('Company', doc.company, 'tax_id')
-			if not tax_id:
-				frappe.throw(_('Tax ID missing for {} in the company document'.format(doc.company)))
+            # seller_name = doc.company
+            # tag = bytes([1]).hex()
+            # length = bytes([len(seller_name)]).hex()
+            # value = seller_name.encode('utf-8').hex()
+            #
+            # tlv_array.append(''.join([tag, length, value]))
 
-			# tax_id = '310122393500003'  #
-			tag = bytes([2]).hex()
-			length = bytes([len(tax_id)]).hex()
-			value = tax_id.encode('utf-8').hex()
-			tlv_array.append(''.join([tag, length, value]))
+            # VAT Number
+            tax_id = frappe.db.get_value('Company', doc.company, 'tax_id')
+            if not tax_id:
+                frappe.throw(_('Tax ID missing for {} in the company document'.format(doc.company)))
 
-			# Time Stamp
-			posting_date = getdate(doc.posting_date)
-			time = get_time(doc.posting_time)
-			seconds = time.hour * 60 * 60 + time.minute * 60 + time.second
-			time_stamp = add_to_date(posting_date, seconds=seconds)
-			time_stamp = time_stamp.strftime('%Y-%m-%dT%H:%M:%SZ')
+            # tag = bytes([2]).hex()
+            # length = bytes([len(tax_id)]).hex()
+            # value = tax_id.encode('utf-8').hex()
+            # tlv_array.append(''.join([tag, length, value]))
 
-			# time_stamp = '2022-04-25T15:30:00Z'  #
-			tag = bytes([3]).hex()
-			length = bytes([len(time_stamp)]).hex()
-			value = time_stamp.encode('utf-8').hex()
-			tlv_array.append(''.join([tag, length, value]))
-			invoice_amount = str(doc.grand_total)
-			if hasattr(doc,"custom_grantee_value"):
-				# Invoice Amount
-				invoice_amount = str(((doc.net_total)+(doc.total_taxes_and_charges - (doc.total_advance*0.15)))  - (doc.total_advance+flt(doc.get("custom_grantee_value"))))
+            # Time Stamp
+            posting_date = getdate(doc.posting_date)
+            time = get_time(doc.posting_time)
+            seconds = time.hour * 60 * 60 + time.minute * 60 + time.second
+            time_stamp = add_to_date(posting_date, seconds=seconds)
+            time_stamp = time_stamp.strftime('%Y-%m-%dT%H:%M:%SZ')
 
-			# invoice_amount = '1000.00'  #
-			tag = bytes([4]).hex()
-			length = bytes([len(invoice_amount)]).hex()
-			value = invoice_amount.encode('utf-8').hex()
-			tlv_array.append(''.join([tag, length, value]))
+            # time_stamp = '2022-04-25T15:30:00Z'  #
+            tag = bytes([3]).hex()
+            length = bytes([len(time_stamp)]).hex()
+            value = time_stamp.encode('utf-8').hex()
+            tlv_array.append(''.join([tag, length, value]))
+            invoice_amount = str(doc.grand_total)
+            if hasattr(doc,"custom_grantee_value"):
+                # Invoice Amount
+                invoice_amount = str(((doc.net_total)+(doc.total_taxes_and_charges - (doc.total_advance*0.15)))  - (doc.total_advance+flt(doc.get("custom_grantee_value"))))
 
-			# VAT Amount
-			vat_amount = str((doc.total_taxes_and_charges - (doc.total_advance*0.15) ))
+            # invoice_amount = '1000.00'  #
+            tag = bytes([4]).hex()
+            length = bytes([len(invoice_amount)]).hex()
+            value = invoice_amount.encode('utf-8').hex()
+            tlv_array.append(''.join([tag, length, value]))
 
-			# vat_amount = '150.00'  #
-			tag = bytes([5]).hex()
-			length = bytes([len(vat_amount)]).hex()
-			value = vat_amount.encode('utf-8').hex()
-			tlv_array.append(''.join([tag, length, value]))
+            # VAT Amount
+            vat_amount = str((doc.total_taxes_and_charges - (doc.total_advance*0.15) ))
 
-			# Joining bytes into one
-			tlv_buff = ''.join(tlv_array)
+            # vat_amount = '150.00'  #
+            tag = bytes([5]).hex()
+            length = bytes([len(vat_amount)]).hex()
+            value = vat_amount.encode('utf-8').hex()
+            tlv_array.append(''.join([tag, length, value]))
 
-			# base64 conversion for QR Code
-			base64_string = b64encode(bytes.fromhex(tlv_buff)).decode()
-			# Creating public url to print format
-			default_print_format = frappe.db.get_value('Property Setter',
-													   dict(property='default_print_format', doc_type=doc.doctype),
-													   "value")
+            # Joining bytes into one
+            tlv_buff = ''.join(tlv_array)
 
-			# System Language
-			language = frappe.get_system_settings('language')
+            # base64 conversion for QR Code
+            base64_string = b64encode(bytes.fromhex(tlv_buff)).decode()
+            # Creating public url to print format
+            default_print_format = frappe.db.get_value('Property Setter',
+                                                       dict(property='default_print_format', doc_type=doc.doctype),
+                                                       "value")
 
-			params = urlencode({
-				'format': default_print_format or 'Standard',
-				'_lang': language,
-				'key': doc.get_signature()
-			})
+            # System Language
+            language = frappe.get_system_settings('language')
 
-			# creating qr code for the url
-			url = f"{frappe.utils.get_url()}/{doc.doctype}/{doc.name}?{params}"
-			qr_image = io.BytesIO()
-			url = qr_create(url, error='L')
-			url = qr_create(base64_string, error='L')
-			url.png(qr_image, scale=2, quiet_zone=1)
-			import random
-			rand_no = random.randint(1, 10)
-			# making file
-			filename = f"QR-CODE-{doc.name}-{rand_no}.png".replace(os.path.sep, "__")
-			_file = frappe.get_doc({
-				"doctype": "File",
-				"file_name": filename,
-				"is_private": 0,
-				"content": qr_image.getvalue(),
-				"attached_to_doctype": doc.get("doctype"),
-				"attached_to_name": doc.get("name"),
-				"attached_to_field": "qr_code"
-			})
+            params = urlencode({
+                'format': default_print_format or 'Standard',
+                '_lang': language,
+                'key': doc.get_signature()
+            })
 
-			delete_qr_code_file(doc,None)
-			_file.save()
-			frappe.db.commit()
-			# assigning to document
-			doc.db_set('qr_code', _file.file_url)
-			doc.db_set('ksa_einv_qr', _file.file_url)
-			doc.notify_update()
-			break
+            # creating qr code for the url
+            url = f"{frappe.utils.get_url()}/{doc.doctype}/{doc.name}?{params}"
+            qr_image = io.BytesIO()
+            url = qr_create(url, error='L')
+            url = qr_create(base64_string, error='L')
+            url.png(qr_image, scale=2, quiet_zone=1)
+            import random
+            rand_no = random.randint(1, 10)
+            # making file
+            filename = f"QR-CODE-{doc.name}-{rand_no}.png".replace(os.path.sep, "__")
+            _file = frappe.get_doc({
+                "doctype": "File",
+                "file_name": filename,
+                "is_private": 0,
+                "content": qr_image.getvalue(),
+                "attached_to_doctype": doc.get("doctype"),
+                "attached_to_name": doc.get("name"),
+                "attached_to_field": "qr_code"
+            })
+
+            delete_qr_code_file(doc,None)
+            _file.save()
+            frappe.db.commit()
+            # assigning to document
+            doc.db_set('qr_code', _file.file_url)
+            doc.db_set('ksa_einv_qr', _file.file_url)
+            doc.notify_update()
+            break
 
 
 
 
-			"""qr_image = io.BytesIO()
-
-			xml = qr_create(xml,error='L',mode='binary', encoding='utf-8',)
-			xml.png(qr_image, scale=2, quiet_zone=1)
-
-			# making file
-			filename = f"QR-CODE-{doc.name}.png".replace(os.path.sep, "__")
-			_file = frappe.get_doc({
-				"doctype": "File",
-				"file_name": filename,
-				"content": qr_image.getvalue(),
-				"is_private": 1
-			})
-
-			_file.save()
-
-			# assigning to document
-			doc.db_set('qr_code', _file.file_url)
-			doc.notify_update()
-
-			break"""
+            """qr_image = io.BytesIO()
+    
+            xml = qr_create(xml,error='L',mode='binary', encoding='utf-8',)
+            xml.png(qr_image, scale=2, quiet_zone=1)
+    
+            # making file
+            filename = f"QR-CODE-{doc.name}.png".replace(os.path.sep, "__")
+            _file = frappe.get_doc({
+                "doctype": "File",
+                "file_name": filename,
+                "content": qr_image.getvalue(),
+                "is_private": 1
+            })
+    
+            _file.save()
+    
+            # assigning to document
+            doc.db_set('qr_code', _file.file_url)
+            doc.notify_update()
+    
+            break"""
 
 def delete_qr_code_file(doc, method):
 	"""Delete QR Code on deleted sales invoice"""
@@ -197,13 +199,105 @@ def update_is_return_reason(doc, method):
 		doc.is_return_reason="مرتجع"
 
 def update_invoices_with_out_qr():
-	invoices = frappe.db.get_all("Sales Ivoice",filters={"docstatus":1,"qr_code":[""," "]},fields="name",order_by="creation desc")
-	if invoices:
-		for invoice in invoices:
-			try:
-				doc = frappe.get_doc("Sales Invocie",invoice["name"])
-				create_qr_code(doc, method=None)
-				print(f"QR Code created for {doc.name}")
-			except Exception as e:
-				print(f"Error in create_qr_code for {doc.name}")
-				frappe.log_error(title="Error in create_qr_code",message=e)
+    invoices = frappe.db.get_all("Sales Invoice",filters={"docstatus":1,"qr_code":["="," "]},fields="name",order_by="creation desc")
+    if invoices:
+        print("=====Starting=======")
+        for invoice in invoices:
+            try:
+                doc = frappe.get_doc("Sales Invocie",invoice["name"])
+                create_qr_codes(doc, method=None)
+                print(f"QR Code created for {doc.name}")
+            except Exception as e:
+                print(f"Error in create_qr_code for {doc.name}")
+                frappe.log_error(title="Error in create_qr_code",message=e)
+
+
+from io import BytesIO
+import base64
+import qrcode
+from datetime import datetime, timezone
+from decimal import Decimal, ROUND_HALF_UP
+import frappe
+from frappe.utils.file_manager import save_file
+
+
+def create_qr_code(doc,method=None):
+    region = get_region(doc.company)
+
+    if region not in ['Saudi Arabia']:
+        return
+    # if QR Code field not present, do nothing
+    if not hasattr(doc, 'qr_code'):
+        return
+
+    attached_to_doctype = doc.doctype
+    attached_to_name = doc.name
+    seller_name=doc.company
+    vat_number = doc.tax_id
+    invoice_datetime = doc.posting_date
+    invoice_total = doc.grand_total
+    vat_total = doc.total_taxes_and_charges
+    # generate QR code
+    png_bytes, base64_tlv = generate_zatca_qr_bytes(
+        seller_name, vat_number, invoice_datetime, invoice_total, vat_total
+    )
+
+    file_name = f"zatca_qr_{attached_to_name}.png"
+
+    # create File doc (private file)
+    file_doc = save_file(
+        fname=file_name,
+        content=png_bytes,
+        dt=attached_to_doctype,
+        dn=attached_to_name,
+        is_private=1,  # or 0 for public file
+    )
+
+    frappe.db.commit()
+
+    return {
+        "file_url": file_doc.file_url,
+        "file_name": file_name,
+        "base64_tlv": base64_tlv
+    }
+
+
+
+def generate_zatca_qr_bytes(seller_name, vat_number, invoice_datetime, invoice_total, vat_total):
+    """Generate ZATCA QR code and return PNG bytes + Base64 string."""
+    def _tlv(tag, value):
+        val = value.encode('utf-8')
+        return bytes([tag, len(val)]) + val
+
+    def _fmt(value):
+        return str(Decimal(str(value)).quantize(Decimal("0.00"), rounding=ROUND_HALF_UP))
+
+    if isinstance(invoice_datetime, datetime):
+        dt = invoice_datetime
+    else:
+        # convert date to datetime at midnight UTC
+        dt = datetime.combine(getdate(invoice_datetime), datetime.min.time())
+
+    dt = dt.replace(tzinfo=timezone.utc)
+    ts = dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+    for field in ['seller_name', 'vat_number', 'ts', 'invoice_total', 'vat_total']:
+        if not locals()[field]:
+            raise ValueError(f"{field} cannot be empty")
+
+    tlv_bytes = b''.join([
+        _tlv(1, seller_name),
+        _tlv(2, vat_number),
+        _tlv(3, ts),
+        _tlv(4, _fmt(invoice_total)),
+        _tlv(5, _fmt(vat_total))
+    ])
+    b64 = base64.b64encode(tlv_bytes).decode('utf-8')
+
+    qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_Q)
+    qr.add_data(b64)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    return buf.getvalue(), b64
